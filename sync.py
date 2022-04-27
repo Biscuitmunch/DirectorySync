64e0d626d1bdad038f563a1d967c6e1aa052ebd7
Skylar <biscuitmunch0@gmail.com>
2022-04-28 00:16:34 +1200

    added timestamps and file reading

--- sync.py
+++ sync.py
@@ -1,12 +1,28 @@
 import datetime
-import pathlib
+from pathlib import Path
+
+#digest_dictionary = {filename: }
+
+def Get_Modified_Time(path):
+    return Path(path).stat().st_mtime
+
+def Digest_Dictionary(path):
+    print("hello")
+
+def Compare_Digest(path):
+    print("hello")
+
+def Add_To_Digest(path):
+    file_list = Path(path).glob('*')
+    for i in file_list:
+        modifiedTime = Get_Modified_Time(i)
+        print(modifiedTime)
 
 dir1User = input()
 dir2User = input()
 
-# path1
-dir1 = pathlib.Path(dir1User)
+# path1 and path1 files
+path1 = Path(dir1User)
+Add_To_Digest(path1)
 
 
-
-#def Time_Formatting()
