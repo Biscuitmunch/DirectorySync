31523b92550c6472384c998af933f4af5eb194fc
Skylar <biscuitmunch0@gmail.com>
2022-04-28 06:01:55 +1200

    Saving to json file

--- sync.py
+++ sync.py
@@ -1,28 +1,51 @@
 import datetime
+from hmac import digest
 from pathlib import Path
+import json
 
 #digest_dictionary = {filename: }
 
 def Get_Modified_Time(path):
     return Path(path).stat().st_mtime
 
-def Digest_Dictionary(path):
-    print("hello")
+def Get_Digest_Dictionary(path):
+    #return json.load(path + r'\.sync')
+    return {}
+
+def Save_Digest(path, file, modified):
+    digest_dictionary = Get_Digest_Dictionary(path)
+
+    if file in digest_dictionary:
+        digest_dictionary[file].append([modified, 500])
+        
+    else:
+        digest_dictionary[file] = [modified, 700]
+
+    with open((str(path) + '.json'), 'w') as js:
+        json.dump(digest_dictionary, js,  indent=4)
+
+def Update_Digest(path):
+
+    file_list = Path(path).glob('*')
+    for file in file_list:
+        
+        if file.is_dir():
+            Update_Digest(file)
+        
+        if file.is_file():
+            modifiedTime = Get_Modified_Time(file)
+            Save_Digest(path, file, modifiedTime)
+
+
 
 def Compare_Digest(path):
     print("hello")
 
-def Add_To_Digest(path):
-    file_list = Path(path).glob('*')
-    for i in file_list:
-        modifiedTime = Get_Modified_Time(i)
-        print(modifiedTime)
-
 dir1User = input()
 dir2User = input()
 
-# path1 and path1 files
+# path1
 path1 = Path(dir1User)
-Add_To_Digest(path1)
+Update_Digest(path1)
 
 
