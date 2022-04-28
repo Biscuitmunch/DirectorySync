681fc50cddbbdbd839113566922b8d21764af869
Skylar <biscuitmunch0@gmail.com>
2022-04-29 04:29:42 +1200

    Implemented hashing and json files finished

--- sync.py
+++ sync.py
@@ -1,29 +1,49 @@
-import datetime
+from array import array
 from hmac import digest
 from pathlib import Path
 import json
+import hashlib
 
-#digest_dictionary = {filename: }
+def Create_Hash(path):
+    with open(path, 'rb') as file:
+        data = file.read()
+        return str(hashlib.sha256(data).hexdigest());
 
 def Get_Modified_Time(path):
     return Path(path).stat().st_mtime
 
 def Get_Digest_Dictionary(path):
-    #return json.load(path + r'\.sync')
-    return {}
+    # check if exists
+    if Path.exists(Path(path)):
+        with open(path) as js:
+            return json.load(js)
+    else:
+        return {}
 
-def Save_Digest(path, file, modified):
-    digest_dictionary = Get_Digest_Dictionary(path)
+
+def Save_Digest(path, file):
+    modified = Get_Modified_Time(file)
+    file_hash = Create_Hash(file)
+
+    # json file locations
+    path_for_json = str(path)
+    array_of_paths = path_for_json.split('/')
+    final_name = array_of_paths[-1]
+    json_path_complete = path_for_json + '/.' + final_name + '.json'
+
+    digest_dictionary = Get_Digest_Dictionary(json_path_complete)
 
     file_name = str(file)
 
     if file_name in digest_dictionary:
-        digest_dictionary[file_name].append([modified, 500])
+        digest_dictionary[file_name].append([modified, file_hash])
         
     else:
-        digest_dictionary[file_name] = [modified, 700]
+        digest_dictionary[file_name] = [[modified, file_hash]]
 
-    with open((str(path) + '.json'), 'w') as js:
+
+
+    with open(json_path_complete, 'w') as js:
         json.dump(digest_dictionary, js,  indent=4)
         js.close()
 
@@ -36,8 +56,7 @@ def Update_Digest(path):
             Update_Digest(file)
         
         if file.is_file():
-            modifiedTime = Get_Modified_Time(file)
-            Save_Digest(path, file, modifiedTime)
+            Save_Digest(path, file)
 
 
 
